5497b8439e255eb7f1712ebd1fc9d40d16549fa9
wylnoword <simondrcula@live.com>
2020-04-16 10:09:45 +0800

    init first kafka

---
+++ Kafka/src/main/java/com/example/kafka/config/KafkaConfig.java
@@ -0,0 +1,7 @@
+package com.example.kafka.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+}
---
+++ Redis/src/main/java/com/process/redis/config/RedisConfig.java
@@ -0,0 +1,7 @@
+package com.process.redis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+}
