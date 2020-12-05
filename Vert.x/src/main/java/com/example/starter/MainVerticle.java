c1aaed9fd804a53959c1d931ad46cf3b9031398c
wangyuliang <wangyuliang@sinoiov.com>
2020-12-05 15:31:55 +0800

    vert.x 初始化

---
+++ Vert.x/src/main/java/com/example/starter/MainVerticle.java
@@ -0,0 +1,35 @@
+package com.example.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Launcher;
+import io.vertx.core.Promise;
+/**
+ * @author wangyuliang
+ * @description //vert.x demo
+ * @date 15:28 2020/12/5
+ * @param
+ * @return
+ */
+public class MainVerticle extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    Launcher.main(new String[] { "run", MainVerticle.class.getName()});
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/json")
+        .end("Hello from Vert.x!");
+    }).listen(8888, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+
+}
